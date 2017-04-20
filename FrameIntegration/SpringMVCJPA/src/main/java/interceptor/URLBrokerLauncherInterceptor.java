05c60a0fdb60c80f1940b9f9bd0d5cf27b80fbca
yuyufeng <416530244@qq.com>
2017-04-20 17:21:48 +0800

    SpringMVCJPA集成

--- FrameIntegration/SpringMVCJPA/src/main/java/interceptor/URLBrokerLauncherInterceptor.java
+++ FrameIntegration/SpringMVCJPA/src/main/java/interceptor/URLBrokerLauncherInterceptor.java
@@ -1,15 +1,12 @@
 package interceptor;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 
 /**
  * Created by yuyufeng on 2017/4/20.
