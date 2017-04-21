4f691f8ee15e178f997f0ac794e90fcdbd00acb0
yuyufeng <416530244@qq.com>
2017-04-21 10:11:55 +0800

    commit

--- JavaBase/src/main/java/demo/java/test/Main.java
+++ JavaBase/src/main/java/demo/java/test/Main.java
@@ -1,15 +1,22 @@
 package demo.java.test;
 
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-	public static void main(String[] args) throws InterruptedException {
-		String no="1,2,3";
-		for(String n:no.split(",")){
-			System.out.println(n);
-		}
-		List<String> list = new ArrayList<String>();
+	public static void main(String[] args) throws InterruptedException, NoSuchAlgorithmException {
+		KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance("RSA");
+		keyPairGen.initialize(1024);
+		KeyPair keyPair = keyPairGen.generateKeyPair();
+		RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+		RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+		System.out.println(publicKey.toString());
+		System.out.println(privateKey.toString());
 
 	}
 }
