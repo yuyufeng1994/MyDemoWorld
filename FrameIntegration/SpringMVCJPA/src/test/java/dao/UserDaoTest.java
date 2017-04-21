6acc37a07a554c8fa2f8f52b8eafd04cfa6f52d2
yuyufeng <416530244@qq.com>
2017-04-21 10:09:19 +0800

    添加分页测试

--- FrameIntegration/SpringMVCJPA/src/test/java/dao/UserDaoTest.java
+++ FrameIntegration/SpringMVCJPA/src/test/java/dao/UserDaoTest.java
@@ -4,6 +4,10 @@ import entity.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -27,10 +31,31 @@ public class UserDaoTest {
     }
 
     @Test
+    public void testSaveList() {
+        for (int i = 10; i < 50; i++) {
+            User user = new User();
+            user.setUserName("用户"+i);
+            User res = userDao.save(user);
+            System.out.println(res);
+        }
+
+    }
+
+    @Test
     public void findAll() {
         List<User> list = (List<User>) userDao.findAll();
         for (User user : list) {
             System.out.println("##" + user);
+        }
+    }
+
+    @Test
+    public void testPage() {
+        Sort sort = new Sort(Sort.Direction.DESC, "userId");
+        Pageable pageable = new PageRequest(1, 10, sort);
+        Page<User> page = userDao.findAll(pageable);
+        for (User user : page.getContent()) {
+            System.out.println(user);
         }
     }
 
