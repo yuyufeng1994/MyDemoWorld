12bbd935e02fe31a3d49e0f28205eeb802475f48
yuyufeng <416530244@qq.com>
2017-06-05 16:42:28 +0800

    commit

---
+++ JavaLibUse/common/src/main/java/common/qrcode/CreateQR.java
@@ -0,0 +1,36 @@
+package common.qrcode;
+
+import com.alibaba.fastjson.JSONObject;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by yuyufeng on 2017/6/5.
+ */
+public class CreateQR {
+    public static void main(String[] args) throws WriterException, IOException {
+        String filePath = "D://";
+        String fileName = "test.png";
+        String content = "http://www.yuyufeng.top";// 内容
+        int width = 200; // 图像宽度
+        int height = 200; // 图像高度
+        String format = "png";// 图像类型
+        Map<EncodeHintType, Object> hints = new HashMap<EncodeHintType, Object>();
+        hints.put(EncodeHintType.CHARACTER_SET, "UTF-8");
+        BitMatrix bitMatrix = new MultiFormatWriter().encode(content,
+                BarcodeFormat.QR_CODE, width, height, hints);// 生成矩阵
+        Path path = FileSystems.getDefault().getPath(filePath, fileName);
+        MatrixToImageWriter.writeToPath(bitMatrix, format, path);// 输出图像
+        System.out.println("输出成功.");
+    }
+}
---
+++ JavaLibUse/common/src/main/java/common/qrcode/DecodeQR.java
@@ -0,0 +1,40 @@
+package common.qrcode;
+
+import com.alibaba.fastjson.JSONObject;
+import com.google.zxing.*;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.common.HybridBinarizer;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by yuyufeng on 2017/6/5.
+ */
+public class DecodeQR {
+    public static void main(String[] args) {
+        String filePath = "D://test.png";
+        BufferedImage image;
+        try {
+            image = ImageIO.read(new File(filePath));
+            LuminanceSource source = new BufferedImageLuminanceSource(image);
+            Binarizer binarizer = new HybridBinarizer(source);
+            BinaryBitmap binaryBitmap = new BinaryBitmap(binarizer);
+            Map<DecodeHintType, Object> hints = new HashMap<DecodeHintType, Object>();
+            hints.put(DecodeHintType.CHARACTER_SET, "UTF-8");
+            Result result = new MultiFormatReader().decode(binaryBitmap, hints);// 对图像进行解码
+            System.out.println("图片中内容：  ");
+            System.out.println("content： " + result.getText());
+            System.out.println("图片中格式：  ");
+            System.out.println("encode： " + result.getBarcodeFormat());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (NotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
